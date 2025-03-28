bba6c9103e18fec8869f04febd2628ec057f5de8
Ken <kdrichmond@gmail.com>
2025-03-28 14:17:56 -0400

    add testing poc

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Caluculator functions
+"""
+
+def add(x, y):
+    """ Add x and y and return the results """
+    return x + y
+
+def subtract(x, y):
+    """ Subtract y from x and return the results """
+    return y - x 
+
+
+
