f4659e2fa706a7e697de11590afe57365b3deeeb
nrkey420 <kdrichmond@gmail.com>
2025-07-17 15:24:23 -0400

    Final Tags API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,16 @@
 """ views for the recipe api's"""
 
-from rest_framework import viewsets
+from rest_framework import(
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 from recipe import serializers
 
@@ -28,3 +34,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """ View for managing tags """
+    serializer_class = serializers.TagsSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Retrieve the tags for the authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
