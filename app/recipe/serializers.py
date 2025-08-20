5a694c2f29bcbb98353a96ac55561fb255d58037
Ken <kdrichmond@gmail.com>
2025-08-20 14:08:51 -0400

    Start Ingredients API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,6 +4,15 @@ from rest_framework import serializers
 
 from core.models import Recipe
 from core.models import Tag
+from core.models import Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """ Serializer for ingredients. """
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 class TagsSerializer(serializers.ModelSerializer):
     """ Serializer for tags. """
