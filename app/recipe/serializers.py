16fd04320a234e17eaba2038c89503595bf0bafb
nrkey420 <kdrichmond@gmail.com>
2025-08-22 14:55:16 -0400

    last image update fix

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -83,7 +83,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     """ Serializer for recipe detail objects. """
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
